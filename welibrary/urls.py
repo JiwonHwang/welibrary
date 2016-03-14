320bcb0d3c5f8c723684ac5d58c3c957426475f1
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-15 06:06:18 +0900

    add '    url('', include('social.apps.django_app.urls', namespace='social')), '

--- welibrary/urls.py
+++ welibrary/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns( '',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
     url(r'', include('note.urls')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
 )
