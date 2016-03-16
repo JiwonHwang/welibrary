f3bb8d43eddf129f471b253a0f1bd2222400b1b4
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-16 16:37:18 +0900

    add url for templates in accoutns app

--- welibrary/urls.py
+++ welibrary/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import  patterns, include, url
-
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 
@@ -10,5 +9,7 @@ urlpatterns = patterns( '',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
     url(r'', include('note.urls', namespace='note')), # namespace='note'
-    url('', include('social.apps.django_app.urls', namespace='social')),
+    # url('', include('social.apps.django_app.urls', namespace='social')),
+    # social apps// why it doesn't start from r? check the error
+    url(r'^accounts/', include('accounts.urls', namespace="accounts")),
 )
